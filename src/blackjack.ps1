89f02d9fb7fc99aa43b33d97eabd0eaa4268d8b3
torsteinnesby <ttnesby@gmail.com>
2021-11-19 07:07:50 +0100

    pwsh result

--- src/blackjack.ps1
+++ src/blackjack.ps1
@@ -7,7 +7,7 @@ param (
 
 $ErrorActionPreference = [System.Management.Automation.ActionPreference]::Stop
 
-$BlackJack = 21
+$BlackJack = { 21 }
 
 $function:cardScore = { param($card)
     switch ($card.value) {
@@ -17,8 +17,9 @@ $function:cardScore = { param($card)
     }
 }
 
-$function:score = {
-    param ([object[]] $doc)
+$function:cardShow = { param($card) $card.suit[0] + $card.value }
+
+$function:docScore = { param ([object[]] $doc)
 
     $function:sloop = { param([object[]]$doc, $sum)
         if ($doc.Count -eq 1) { $sum + (cardScore $doc[0]) }
@@ -28,48 +29,67 @@ $function:score = {
     (sloop $doc 0)
 }
 
-$function:noCards = { param([object[]]$doc) $doc.Count -eq 0 }
-$function:isLT17 = { param([object[]]$doc) (score $doc) -lt 17 }
-$function:isBJ = { param([object[]]$doc) (score $doc) -eq $BlackJack }
-$function:isGTBJ = { param([object[]]$doc) (score $doc) -gt $BlackJack }
+$function:docShow = { param([object[]]$doc)
 
-function recursive {
+    $function:hloop = { param([object[]]$doc, $show)
+        if ($doc.Count -eq 1) { $show + (cardShow $doc[0]) }
+        else { hloop ($doc[1..$doc.Count]) ($show + (cardShow $doc[0]) + ',') }
+    }
+    
+    (hloop $doc '')
+}
+
+$function:noCards = { param([object[]]$doc) $doc.Count -eq 0 }
+$function:isLT17 = { param([object[]]$doc) (docScore $doc) -lt 17 }
+$function:isBJ = { param([object[]]$doc) (docScore $doc) -eq (&$BlackJack) }
+$function:isGTBJ = { param([object[]]$doc) (docScore $doc) -gt (&$BlackJack) }
+
+$function:giveReceiveX = { param($x, [object[]]$g, [object[]]$r)
+    $t = $g[0..($x - 1)]
+    ($g[2..$g.Count]), ($r + $t)
+}
+
+$function:bjResult = { param($winner, [object[]]$me, [object[]]$magnus)
+    [ordered]@{  
+        winner = $winner
+        me     = [ordered]@{ score = (docScore $me); cards = (docShow $me) } 
+        magnus = [ordered]@{ score = (docScore $magnus); cards = (docShow $magnus) } 
+    } | ConvertTo-Json
+}
+
+function blackjack {
     param(
         $me = @(),
         $magnus = @(),
         $doc = ((Invoke-WebRequest -Uri $urlKortstokk).Content | ConvertFrom-Json)
     )
 
-    function bjloop {
-        param($me, $magnus, $doc)
+    $function:bjloop = { param($me, $magnus, $doc)
 
-        $function:giveReceiveX = { param($x, [object[]]$g, [object[]]$r)
-            $t = $g[0..($x - 1)]
-            ($g[2..$g.Count]), ($r + $t)
-        }
-
-        $function:meX = { param($x)
+        $function:giveMe = { param($x)
             $d, $e = (giveReceiveX $x $doc $me)
             bjloop $e $magnus $d
         }
 
-        $function:magnusX = { param($x)
+        $function:giveMagnus = { param($x)
             $d, $a = (giveReceiveX $x $doc $magnus)
             bjloop $me $a $d
         }
 
-        $function:isLEMe = { param([object[]]$doc) (score $doc) -le (score $me) }
+        $MagnusLEMe = { (docScore $magnus) -le (docScore $me) }
+        $winMe = { (bjResult 'me' $me $magnus) }
+        $winMagnus = { (bjResult 'magnus' $me $magnus) }
 
         switch (0) {
-            { (noCards $me) } { meX 2; break }
-            { (noCards $magnus) } { magnusX 2; break }
-            { (isBJ $me) } { return @{w = "me"; me = $me; magnus = $magnus } }
-            { (isBJ $magnus) } { return @{w = "magnus"; me = $me; magnus = $magnus } }
-            { (isLT17 $me) } { meX 1; break }
-            { (isGTBJ $me) } { return @{w = "magnus"; me = $me; magnus = $magnus } }
-            { (isLEMe $magnus) } { magnusX 1; break }
-            { (isGTBJ $magnus) } { return @{w = "me"; me = $me; magnus = $magnus } }
-            Default { return @{w = "magnus"; me = $me; magnus = $magnus } }
+            { (noCards $me) } { giveMe 2; break }
+            { (noCards $magnus) } { giveMagnus 2; break }
+            { (isBJ $me) } { return (&$winMe) }
+            { (isBJ $magnus) } { return (&$winMagnus) }
+            { (isLT17 $me) } { giveMe 1; break }
+            { (isGTBJ $me) } { return (&$winMagnus) }
+            { (&$MagnusLEMe) } { giveMagnus 1; break }
+            { (isGTBJ $magnus) } { return (&$winMe) }
+            Default { return (&$winMagnus) }
         }
     }
 
