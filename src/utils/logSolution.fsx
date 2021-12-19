7def72c5ebf725f6d0e42afca2d5cd6f198744b5
torsteinnesby <ttnesby@gmail.com>
2021-12-19 21:21:52 +0100

    log as json

--- src/utils/logSolution.fsx
+++ src/utils/logSolution.fsx
@@ -2,11 +2,13 @@
 
 open NLog
 
-(
-    Config.LoggingConfiguration(),
-    new Targets.ColoredConsoleTarget("Console")
-)
-|> fun (conf, target) ->
-        //target.Layout <- @""
-        conf.AddRule(LogLevel.Debug, LogLevel.Fatal, target)
-        LogManager.Configuration <- conf
+//(
+//    Config.LoggingConfiguration(),
+//    new Targets.ColoredConsoleTarget("Console")
+//)
+//|> fun (conf, target) ->
+//        //target.Layout <- @""
+//        conf.AddRule(LogLevel.Debug, LogLevel.Fatal, target)
+//        LogManager.Configuration <- conf
+
+LogManager.Configuration <- Config.XmlLoggingConfiguration($"{__SOURCE_DIRECTORY__}/NLog.config")
